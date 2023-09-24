5896ccee03b431218a45b5852b95542275655a89
Joe Polny <joepolny@gmail.com>
2023-09-24 10:32:53 -0400

    proper pack

--- bin/algokit-generate-component.js
+++ bin/algokit-generate-component.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint-disable no-console */
 import path from 'path';
 import { readFileSync, writeFileSync } from 'fs';
@@ -5,12 +6,12 @@ import generate from '../src';
 
 const outDir = path.join('test', 'components');
 const inputString = readFileSync('./test/Dao.json', 'utf8');
-
 const result = generate(inputString);
 
 result.forEach((r) => writeFileSync(path.join(outDir, `${r.name}.tsx`), r.content));
 
 console.log('If you are using the Algokit react template add these imports to your App.tsx to use the components:\n');
+
 result.forEach((r) => {
   console.log(`import ${r.name} from '${path.join(outDir, `${r.name}.tsx`)}'`);
 });
